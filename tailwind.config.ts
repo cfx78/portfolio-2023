ad888c6eafe0daaba604fac290c20ab949758854
cfx78 <104265674+cfx78@users.noreply.github.com>
2023-11-27 17:12:12 -0600

    adding theme toggle

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,62 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	daisyui: {
+		themes: [
+			{
+				dark: {
+					primary: '#011627',
+
+					foreground: '#41ead4',
+
+					accent: '#f71735',
+
+					neutral: '#fdfffc',
+
+					info: '#2272FF',
+
+					success: '#508484',
+
+					warning: '#FC9E4F',
+
+					error: '#CC2936',
+				},
+				light: {
+					primary: '#fdfffc',
+
+					foreground: '#41ead4',
+
+					accent: '#f71735',
+
+					neutral: '#fdfffc',
+
+					info: '#2272FF',
+
+					success: '#508484',
+
+					warning: '#FC9E4F',
+
+					error: '#CC2936',
+				},
+			},
+
+			'dark',
+		],
+	},
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+		},
+	},
+	plugins: [require('@tailwindcss/typography'), require('daisyui')],
+};
+export default config;
